148769d0f97dcddc388f1e40ab37a2123e6f31e9
Jose <wildchamo@gmail.com>
2021-07-23 12:56:58 -0500

    creación del triangulo y el calculo de su perimetro, area

--- src/pruebajs/taller1/figuras.js
+++ src/pruebajs/taller1/figuras.js
@@ -1,4 +1,5 @@
 //Código del cuadrado
+console.group("Cuadrado")
 
 const ladoCuadrado= 5
 console.log("los lados del cuadrado mide: " +ladoCuadrado);
@@ -9,8 +10,34 @@ console.log("el perimetro del cuadrado es: " +perimetroCuadrado);
 const areaCuadrado= ladoCuadrado* ladoCuadrado;
 console.log("el área del cuadrado es: "+areaCuadrado +"centimetros cuadrados");
 
-//Código del triangulo
 
+console.groupEnd();
+//Código del triangulo
+console.group("Triangulo")
 const ladoTriangulo1=6;
 const ladoTriangulo2=6;
 const baseTriangulo=4;
+const alturaTriangulo= 5.5;
+
+console.log(
+    "los lados del triagulo miden: "
+    +ladoTriangulo1+ " cm,"+
+    ladoTriangulo2+ " cm,"+ 
+    ", su base es: "+baseTriangulo +" cm"
+    + " y su altura es:"+alturaTriangulo)
+    
+
+const perimetroTriangulo= ladoTriangulo1+ladoTriangulo2+baseTriangulo;
+console.log("el perimetro del triangulo es: "+ perimetroTriangulo);
+
+const areaTriangulo = ( baseTriangulo*alturaTriangulo)/2;
+console.log("el area del triangulo es: " +areaTriangulo);
+
+console.groupEnd();
+
+
+console.group("Circulos")
+
+
+
+console.groupEnd();
