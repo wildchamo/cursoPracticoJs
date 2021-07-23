b2bdfa5e55bb619d993525ab6193aad15fcf2e53
Jose <wildchamo@gmail.com>
2021-07-23 12:45:42 -0500

    creación del cuadrado y el calculo de su perimetro, area

---
+++ src/pruebajs/taller1/figuras.js
@@ -0,0 +1,16 @@
+//Código del cuadrado
+
+const ladoCuadrado= 5
+console.log("los lados del cuadrado mide: " +ladoCuadrado);
+
+const perimetroCuadrado= ladoCuadrado*4;
+console.log("el perimetro del cuadrado es: " +perimetroCuadrado);
+
+const areaCuadrado= ladoCuadrado* ladoCuadrado;
+console.log("el área del cuadrado es: "+areaCuadrado +"centimetros cuadrados");
+
+//Código del triangulo
+
+const ladoTriangulo1=6;
+const ladoTriangulo2=6;
+const baseTriangulo=4;
