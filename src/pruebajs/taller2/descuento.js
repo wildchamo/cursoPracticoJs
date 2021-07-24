6b90e83c829e9b49a2ee76a9624b897b59122884
Jose <wildchamo@gmail.com>
2021-07-24 11:40:03 -0500

    calculo del precio con descuento con js

--- src/pruebajs/taller2/descuento.js
+++ src/pruebajs/taller2/descuento.js
@@ -1,2 +1,14 @@
 const precioOriginal= 100;
 const descuento=15;
+
+
+const porcentajeConDescuento= 100- descuento;
+
+const precioConDescuento= (precioOriginal*porcentajeConDescuento)/100;
+
+console.log({
+    precioOriginal,
+    descuento,
+    porcentajeConDescuento,
+    precioConDescuento
+});
