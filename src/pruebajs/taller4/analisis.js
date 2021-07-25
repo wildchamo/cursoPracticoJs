b736fe0031b64bffcc6dd4d252f3289922d061cc
Jose <wildchamo@gmail.com>
2021-07-25 15:34:26 -0500

    importo la función que calcula la media

--- src/pruebajs/taller4/analisis.js
+++ src/pruebajs/taller4/analisis.js
@@ -10,14 +10,20 @@ function esPar(numero){
     return (numero%2 === 0)
 }
 
+function calcularMediaAritmetica(lista){
+     let sumaLista=0;
+     for(let i=0; i<lista.length; i++){
+         sumaLista += lista[i];
+     }
+
 function medianaSalariosCol(lista){
     const mitad= parseInt(lista.length/2);
     
     if(esPar(lista.length)){
         const personaMitad1= lista[mitad-1];
         const personaMitad2= lista[mitad];
-
-
+        const personaMitad= calcularMediaAritmetica([personaMitad1,personaMitad2]);
+        return personaMitad;
         
     }else{
         const personaMitad=lista[mitad];
