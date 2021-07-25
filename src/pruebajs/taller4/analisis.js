84dc2a2a49869b4c37a8825251569d223bcd1fc5
Jose <wildchamo@gmail.com>
2021-07-25 15:50:00 -0500

    arreglo error en la función calcularMediaAritmetica

--- src/pruebajs/taller4/analisis.js
+++ src/pruebajs/taller4/analisis.js
@@ -1,33 +1,40 @@
-const salariosCol= colombia.map(function(persona){
+const salariosCol = colombia.map(function (persona) {
     return persona.salary;
 })
 
-const salariosColSorted=salariosCol.sort(function(salarioA,salarioB){
-    return salarioA-salarioB;
-});
+const salariosColSorted = salariosCol.sort(
+    function (salarioA, salarioB) {
+        return salarioA - salarioB;
+    });
 
-function esPar(numero){
-    return (numero%2 === 0)
+function esPar(numero) {
+    return (numero % 2 === 0)
 }
 
-function calcularMediaAritmetica(lista){
-     let sumaLista=0;
-     for(let i=0; i<lista.length; i++){
-         sumaLista += lista[i];
-     }
+function calcularMediaAritmetica(lista) {
+    const sumaLista = lista.reduce(
+      function (valorAcumulado = 0, nuevoElemento) {
+        return valorAcumulado + nuevoElemento;
+      }
+    );
+    const promedioLista = sumaLista / lista.length;
+    return promedioLista;
+  }
 
-function medianaSalariosCol(lista){
-    const mitad= parseInt(lista.length/2);
-    
-    if(esPar(lista.length)){
-        const personaMitad1= lista[mitad-1];
-        const personaMitad2= lista[mitad];
-        const personaMitad= calcularMediaAritmetica([personaMitad1,personaMitad2]);
+function medianaSalariosCol(lista) {
+    const mitad = parseInt(lista.length / 2);
+
+    if (esPar(lista.length)) {
+        const personaMitad1 = lista[mitad - 1];
+        const personaMitad2 = lista[mitad];
+
+        let personaMitad = calcularMediaAritmetica([personaMitad1, personaMitad2]);
         return personaMitad;
-        
-    }else{
-        const personaMitad=lista[mitad];
+
+    } else {
+        const personaMitad = lista[mitad];
         return personaMitad;
     }
 }
 
+console.log(medianaSalariosCol(salariosColSorted));
