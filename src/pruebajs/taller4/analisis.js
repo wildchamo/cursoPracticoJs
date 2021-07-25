ec15413fdd96ed842324754594315cbb04513abc
Jose <wildchamo@gmail.com>
2021-07-25 15:24:39 -0500

    sort de los salarios de mayor a menor

--- src/pruebajs/taller4/analisis.js
+++ src/pruebajs/taller4/analisis.js
@@ -1,3 +1,7 @@
 const salariosCol= colombia.map(function(persona){
     return persona.salary;
 })
+
+const salariosColSorted=salariosCol.sort(function(salarioA,salarioB){
+    return salarioA-salarioB;
+})
