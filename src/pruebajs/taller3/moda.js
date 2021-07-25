b18892718d9b93dc4f2640f1b0f857c3af8e8bcb
Jose <wildchamo@gmail.com>
2021-07-25 15:05:03 -0500

    calculo de la moda con el metodo sort

--- src/pruebajs/taller3/moda.js
+++ src/pruebajs/taller3/moda.js
@@ -1,16 +1,25 @@
 //calcular la moda con js
 
-lista3= [2,2,2,2,2,2,21,1,1,1,1,4,4,5,7,4,1,2,12,4,5,4,3,1,2]
+lista3 = [2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 4, 4, 5, 7, 4, 1, 2, 2, 4, 5, 4, 3, 1, 2]
 
-const lista3Count={};
+const lista3Count = {};
 
 lista3.map(
-    function(elemento){
+    function (elemento) {
 
-        if(lista3Count[elemento]){
+        if (lista3Count[elemento]) {
             lista3Count[elemento] += 1;
-        }else{
-            lista3Count[elemento]=1;
+        } else {
+            lista3Count[elemento] = 1;
         }
     }
 );
+
+const lista3Array = Object.entries(lista3Count).sort(
+    function (valorAcumulado, nuevoValor) {
+        return valorAcumulado[1] - nuevoValor[1]
+    }
+);
+
+
+const moda= lista3Array[lista3Array.length - 1]
